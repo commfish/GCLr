5b1a5bbe40c0cc39c8f9c1e804dbfb83928cc608
awbarclay <andy.barclay@alaska.gov>
2023-05-19 12:08:39 -0800

    added package documentation

--- R/base_eval_sample_sizes.r
+++ R/base_eval_sample_sizes.r
@@ -1,50 +1,34 @@
-base_eval_sample_sizes <- function(sillyvec, group_names, groupvec, mixsize, scenarios = round(seq(.01, 1, .01), 2), maxprop = 0.5, seed = 56){
+#' Get sample sizes for rubias baseline evaluation.
+#' 
+#' This function creates a tibble of sample sizes for creating baseline and mixture files for baseline evaluation tests.
+#' 
+#' @param sillyvec a character vector of silly codes without the ".gcl" extension (e.g. sillyvec <- c("KQUART06","KQUART08","KQUART10")). 
+#' 
+#' @param group_names character vector of group names the length of max(groupvec)
+#' 
+#' @param groupvec a numeric vector indicating the group affiliation of each pop in sillyvec
+#' 
+#' @param mixsize a numeric vector of length 1; the sample size of each test mixture
+#' 
+#' @param scenarios a numeric vector of proportions to test for each group
+#' 
+#' @param  maxprop a numeric vector of length 1; the maximum proportion of baseline individuals to select from each reporting group (see details). 
+#' 
+#' @param seed integer to set the seed for rmultinom(), so sample sizes are reproducible
+#' 
+#' @details The maxprop argument should be set to avoid oversampling populations, so the allele frequencies . For example, if maxprop = 0.5, the output tibble will only contain scenarios where sample sizes for the test_group do no exceed 50% of the fish in the baseline for that group.
+#'
+#' @export
+#' 
+#' @return a tibble with 4 variables: test_group, scenario, repunit, and samps. For each test_group and scenario, the number of rows will be length(group_names).
+#' 
+#' @examples
+#' 
+#' attach("V:/Analysis/2_Central/Chinook/Susitna River/Susitna_Chinook_baseline_2020/Susitna_Chinook_baseline_2020.Rdata")
+#' Final_Pops <- Final_Pops %>% mutate(group = factor(group, levels = unique(group)))
+#' base_eval_sample_sizes(sillyvec = Final_Pops$silly, group_names = Final_Pops$group %>% levels(), groupvec = Final_Pops$group %>% as.numeric(), scenarios = round(seq(.01, 1, .01), 2), mixsize = 200, maxprop = 0.5, seed = 123)
 
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #
-  #   This function creates a tibble of sample sizes for creating baseline and mixture files for baseline evaluation tests.
-  #   
-  #   The output from this function can be used by create_rubias_base_eval() to write out mixture and baseline files
-  #   for baseline evaluation tests in rubias.
-  #
-  # Inputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #   
-  #   sillyvec - a vector of silly codes without the ".gcl" extention (e.g. sillyvec <- c("KQUART06","KQUART08","KQUART10")). 
-  #
-  #   group_names - character vector of group names the length of max(groupvec)
-  #
-  #   groupvec - numeric vector indicating the group affiliation of each pop in sillyvec
-  #
-  #   mixsize - the sample size of each test mixture
-  #   
-  #   scenarios - a numeric vector of proportions to test for each group. 
-  # 
-  #   maxprop - a numberic vector of length 1. The sets the maximum proportion of baseline individuals that can be 
-  #             selected from each reporting group. For example, if maxprop = 0.5, the output tibble will only contain scenarios
-  #             where sample sizes for the test_group do no exceed 50% of the fish in the baseline for that group.
-  #
-  #   seed - integer to set the seed for rmultinom(), so sample sizes are reproducible
-  #
-  # Outputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #
-  #  The output of this function is a tibble with 4 variables: test_group, scenario, repunit, and samps
-  #  For each test_group and scenario, the number of rows will be length(group_names).
-  #
-  #  Here's an example output tibble for testing Upper_Susitna at 1% of the mixture..  
-  #     test_group    scenario repunit         samps
-  #     <chr>            <dbl> <chr>           <dbl>
-  #     Upper_Susitna     0.01 Upper_Susitna       2  #Notice Upper_Susitna has 2 samples selected for a 200 sample mixuture (2/200 = 0.01)
-  #     Upper_Susitna     0.01 Chulitna           39  #The remaining 198 samples are spread across the other reporting groups (repunits) 
-  #     Upper_Susitna     0.01 Talkeetna          47
-  #     Upper_Susitna     0.01 Eastern_Susitna    40
-  #     Upper_Susitna     0.01 Deshka             35
-  #     Upper_Susitna     0.01 Yentna             37
-  #
-  # Example~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #  attach("V:/Analysis/2_Central/Chinook/Susitna River/Susitna_Chinook_baseline_2020/Susitna_Chinook_baseline_2020.Rdata")
-  #  Final_Pops <- Final_Pops %>% mutate(group = factor(group, levels = unique(group)))
-  #  base_eval_sample_sizes(sillyvec = Final_Pops$silly, group_names = Final_Pops$group %>% levels(), groupvec = Final_Pops$group %>% as.numeric(), scenarios = round(seq(.01, 1, .01), 2), mixsize = 200, maxprop = 0.5, seed = 123)
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+base_eval_sample_sizes <- function(sillyvec, group_names, groupvec, mixsize, scenarios = round(seq(.01, 1, .01), 2), maxprop = 0.5, seed = 56){
   
   
   if(sum(str_detect(group_names, "\\W"))>0){
--- R/confusion_matrix.r
+++ R/confusion_matrix.r
@@ -1,55 +1,46 @@
+#' Create confusion matrices 
+#'
+#' This function is a wrapper for rubias::self_assign(). It takes the genetic likelihood profile produced by rubias::self_assign() and summarizes it into tibbles at the group by group, group by pop, and pop by pop level. This genetic likelihood profile is intended to show how well populations/reporting groups are differentiated by the markers at hand. These probabilities can give an idea of potential misallocation and where. This function can aid in marker selection.
+#'
+#' @param reference A rubias baseline object created by create_rubias_base().
+#' 
+#' @param gen_start_col The first column containing genetic data in the baseline object. This will usually be the default unless the baseline object is modified with additional variables.
+#'
+#' @param output A character vector of summary tibbles you want to produce. Must be one or more of the following: "pop_pop", "pop_group", "group_group". The default is "group_group".
+#'
+#' @return Named list of tibble(s) in long format:
+#'   \itemize{
+#'     \item \code{group_group}: A tibble containing the following variables:
+#'       \itemize{
+#'         \item \code{repunit}: The known repunit (aka reporting group).
+#'         \item \code{inferred_repunit}: The inferred repunit.
+#'         \item \code{mean_group_group_scaled_like}: The average probability of each individual from a group originating from a pop in that group.
+#'       }
+#'     \item \code{pop_group}: A tibble containing the following variables:
+#'       \itemize{
+#'         \item \code{collection}: The known population.
+#'         \item \code{inferred_repunit}: The inferred repunit.
+#'         \item \code{mean_pop_group_scaled_like}: The average probability of each individual from a pop originating from a pop in that group.
+#'       }
+#'     \item \code{pop_pop}: A tibble containing the following variables:
+#'       \itemize{
+#'         \item \code{pop}: The known population.
+#'         \item \code{inferred_pop}: The inferred repunit.
+#'         \item \code{mean_pop_pop_scaled_like}: The average probability of each individual from a pop originating from a pop.
+#'       }
+#'   }
+#'
+#' @examples
+#' baseline <- read_csv("V:/Analysis/5_Coastwide/Chum/NPen2WA_Chum_baseline/rubias/baseline/NPen2Wa_Chum_227pops_91loci_base.csv")
+#' ConfusionMatrices_out <- confusion_matrix(reference = baseline , gen_start_col = 5, output = c("group_group", "pop_group", "pop_pop"))
+#'
+#' @import rubias
+#' @import dplyr
+#' @import purrr
+#'
+#' @export
+
 confusion_matrix <- function(reference, gen_start_col = 5, output = c("group_group", "pop_group", "pop_pop")[1]){
-  
-  ####################################################################################################################################################################################################################################################################
-  #
-  # This function is a wrapper for rubias::self_assign()
-  # It takes the genetic likelihood profile produced by rubias::self_assign()
-  # and summarizes into tibbles at the group by group, group by pop, and pop by pop level.
-  # This genetic likelihood profile is intended to show how well populations / 
-  # reporting groups are differentiated by the markers at hand. These probabilities
-  # can give an idea of potential misallocation and where.
-  # This function can aid in marker selection.
-  # 
-  # Input parameters~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # 
-  # reference - a rubias baseline object created by create_rubias_base()
-  #
-  # gen_start_col - the first column containing genetic data in the baseline object. 
-  #                 This will usually be the default, unless the baseline object is 
-  #                 modified with additional variables.
-  # 
-  # output - a character vector of summary tibbles you want to produce. Must be one or 
-  #          more of the following: "pop_pop", "pop_group", "group_group"
-  #          The default is "group_group".
-  #
-  # Output~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Named List of tibble(s) in long format:
-  #
-  #  group_group - a tibble containing the following variables:
-  #         1) repunit - the known repunit(aka reporting group)
-  #         2) inferred_repunit - the inferred repunit 
-  #         3) mean_group_group_scaled_like - the average probability of each individual from a group originating from a pop in that group
-  #
-  #  pop_group - a tibble containing the following variables:
-  #         1) collection - the known population
-  #         2) inferred_repunit - the inferred repunit 
-  #         3) mean_pop_group_scaled_like - the average probability of each individual from a pop originating from a pop in that group
-  #         
-  #  pop_pop - a tibble containing the following variables:
-  #         1) pop - the known population
-  #         2) inferred_pop - the inferred repunit 
-  #         3) mean_pop_pop_scaled_like - the average probability of each individual from a pop originating from a pop
-  #
-  #  **Note: to visualize the output from this function in a heatmap (ggplot2::geom_tile) use plot_confusion_matrix.GCL()**
-  # 
-  # Example~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #
-  # baseline <- read_csv("V:/Analysis/5_Coastwide/Chum/NPen2WA_Chum_baseline/rubias/baseline/NPen2Wa_Chum_227pops_91loci_base.csv") 
-  # 
-  # ConfusionMatrices_out <- confusion_matrix(reference = baseline , gen_start_col = 5, output = c("group_group", "pop_group", "pop_pop"))
-  #
-  ####################################################################################################################################################################################################################################################################
-  
   
   start <- Sys.time()
   
