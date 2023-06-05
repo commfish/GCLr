f5ea9585e9e0322d1afa1f4665096f4660a90455
awbarclay <andy.barclay@alaska.gov>
2023-06-05 11:46:11 -0800

    removed drv object

--- R/drv.R
+++
@@ -1,6 +0,0 @@
-#' Oracle Driver for RJDBC
-#' 
-#' This is the oracle driver that is used by all `GCLr` functions that use the `RJDBC` package to pull data rom Loki.
-#' 
-#' @details This object was created from the ojdbc8.jar file and saved as a data object in the `GCLr` package.
-"drv"
