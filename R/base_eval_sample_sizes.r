02d6f42e5e0a84e42a4ee6008f886524a6f808a3
awbarclay <andy.barclay@alaska.gov>
2023-06-07 12:04:01 -0800

    Edited example

--- R/base_eval_sample_sizes.r
+++ R/base_eval_sample_sizes.r
@@ -22,9 +22,9 @@
 #' 
 #' @examples
 #' 
-#' attach("V:/Analysis/2_Central/Chinook/Susitna River/Susitna_Chinook_baseline_2020/Susitna_Chinook_baseline_2020.Rdata")
-#' Final_Pops <- Final_Pops %>% mutate(group = factor(group, levels = unique(group)))
-#' base_eval_sample_sizes(sillyvec = Final_Pops$silly, group_names = Final_Pops$group %>% levels(), groupvec = Final_Pops$group %>% as.numeric(), scenarios = round(seq(.01, 1, .01), 2), mixsize = 200, maxprop = 0.5, seed = 123)
+#' 
+#' 
+#' base_eval_sample_sizes(sillyvec = GCLr::baseline, group_names = Final_Pops$group %>% levels(), groupvec = Final_Pops$group %>% as.numeric(), scenarios = round(seq(.01, 1, .01), 2), mixsize = 200, maxprop = 0.5, seed = 123)
 #'
 #' @aliases BaselineEvalSampleSizes.GCL
 #'
