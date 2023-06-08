7ed8ca596d3dca8be44ed073e59abe43fad0406a
awbarclay <andy.barclay@alaska.gov>
2023-06-08 13:44:41 -0800

    added package to select

--- R/confusion_matrix.r
+++ R/confusion_matrix.r
@@ -40,7 +40,7 @@ confusion_matrix <- function(reference, gen_start_col = 5, output = c("group_gro
   start <- Sys.time()
   
   rubias_sa <- rubias::self_assign(reference = baseline, gen_start_col = gen_start_col) %>% 
-    select(-missing_loci, -n_miss_loci, -n_non_miss_loci, -z_score, -log_likelihood)
+    dplyr::select(-missing_loci, -n_miss_loci, -n_non_miss_loci, -z_score, -log_likelihood)
   
   pop_pop <- function(rubias_sa){
     
