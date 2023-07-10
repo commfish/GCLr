a04c495d7e36699075a6deee6f805df9613ad779
bhsu <bobby.hsu@alaska.gov>
2023-07-10 15:28:33 -0800

    update

--- R/bayes_input_copy_paste.r
+++ R/bayes_input_copy_paste.r
@@ -1,37 +1,33 @@
+#' Copy and Paste BAYES Input Files
+#'
+#' This function copies the BAYES input files (.ctl, .mix) from the source directory
+#' and pastes them into the target directory to run BAYES.
+#'
+#' @param origindir Original directory of files.
+#'   A character vector indicating where BAYES files are located.
+#' @param targetdir Target directory structure.
+#'   A character vector indicating where to paste BAYES files (i.e., on the server).
+#' @param mixvec Mixtures to be moved.
+#'   A character vector indicating which mixtures to move.
+#' @param i Bayes folder identifier.
+#'   Indicates which "Bayes" folder to start putting files into.
+#'   Default is to put the first mixture into "Bayes A", second into "Bayes B", etc.
+#'   This function assumes that the Bayes folders on the V drive are listed alphabetically.
+#'
+#' @return This function has no explicit return value. It copies and pastes the files onto the server.
+#'
+#' @examples
+#' \dontrun{
+#' bayes_input_copy_paste(origindir = "V:/Analysis/4_Westward/Sockeye/KMA Commercial Harvest 2014-2016/Mixtures/BAYES/Late August 89loci",
+#'                        targetdir = "C:/Users/krshedd/BAYES", 
+#'                        mixvec = dget(file = "V:/Analysis/4_Westward/Sockeye/KMA Commercial Harvest 2014-2016/Mixtures/Objects/LateAugustMixtures2014Strata.txt"),
+#'                        i = "A")
+#'
+#' }
+#'
+#' @export
+#' 
 bayes_input_copy_paste <- function(origindir, targetdir, mixvec, i = "A"){
-  
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # This function will copy the BAYES input files (.ctl, .mix) from the V drive
-  # and paste them onto the server to run BAYES.
-  # 
-  # Input parameters~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # 
-  # origindir = original directory of files
-  #   ~ A character vector indicating where BAYES files are on the V drive.
-  # targetdir = target directory structure
-  #   ~ A character vector indicating where you want BAYES files to go (i.e. on server).
-  # mixvec = mixtures you want to move
-  #   ~ A character vector indicating which mixtures you want to move.
-  # i = "A"
-  #   ~ Indicates which "Bayes" folder to start with putting files into.
-  #     Default is to put 1st mixture into "Bayes A", second into "Bayes B", etc.
-  #     This function assumes you have your Bayes folders on the V drive listed
-  #     alphabetically.
-  # 
-  # Output~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # No output, just copies files and pastes them on to the server for you.
-  #
-  # Example~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #   source("https://raw.githubusercontent.com/krshedd/GCL-R-Scripts/master/bayes_input_copy_paste.R")
-  #   
-  #   bayes_input_copy_paste(origindir="V:/Analysis/4_Westward/Sockeye/KMA Commercial Harvest 2014-2016/Mixtures/BAYES/Late August 89loci",
-  #                           targetdir="C:/Users/krshedd/BAYES", 
-  #                           mixvec=dget(file="V:/Analysis/4_Westward/Sockeye/KMA Commercial Harvest 2014-2016/Mixtures/Objects/LateAugustMixtures2014Strata.txt"),
-  #                           i = "A")
-  #    
-  # Created by Kyle Shedd Fri Mar 27 16:05:57 2015
-  # Updated by Kyle Shedd Tue Apr 05 15:37:24 2016 in order to generalize and better document
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
   controlfiles <- list.files(path = paste(origindir, "/Control", sep = ""), pattern = ".ctl", full.names = TRUE, recursive = FALSE) # All control files in origindir
  
