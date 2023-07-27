acb0892930dd484caa72d1b5eca103e0b4e334d8
awbarclay <andy.barclay@alaska.gov>
2023-07-27 11:43:58 -0800

    remove_ids - Tyler documented but could not push to GitHub - A.B.

--- R/remove_ids.r
+++ R/remove_ids.r
@@ -1,33 +1,42 @@
-remove_ids <- function(silly, IDs){
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #   This function removes "IDs" (FK_FISH_ID) from the "*.gcl" object associated with "silly".
-  #
-  # Inputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #   
-  #   silly - a silly code without the ".gcl" extention.
-  #
-  #   IDs - a numeric or character vector of FK_FISH_ID's to remove from "silly".
-  #
-  # Outputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #  
-  #  Assigns "*.gcl" objects with removed IDs to the current workspace. 
-  #
-  #  The function returns a tibble with 3 variables: 
-  #                  SILLY_CODE <chr> = the silly with IDs removed 
-  #                  IDs <list> = the IDs removed
-  #                  is_empty <lgl> = were all IDs removed?
-  #
-  # Examples~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #  password <- "************"
-  #
-  #  create_locuscontrol(markersuite = "Sockeye2011_96SNPs", username = "awbarclay", password = password)
-  #  sillyvec <- c("SMCDO03", "SNEVA13")
-  #  loki2r(sillyvec = sillyvec, username = "awbarclay", password = password)
-  #  remove_ind_miss_loci(sillyvec = sillyvec)
-  #
-  #  remove_ids(silly = "SMCDO03", IDs = 1:10)
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Remove IDs from the ".gcl" object associated with "silly"
+#'
+#' This function removes specific IDs from a Silly GCL object based on the provided 'IDs' vector.
+#'
+#' @param silly Character string specifying the name of the Silly GCL object. The function will attempt to find the object with the name 'silly.gcl'.
+#' @param IDs Numeric or character vector containing the IDs to be removed from the Silly GCL object. If 'IDs' is a character vector, it will be converted to numeric.
+#'
+#' @return A tibble with information about the removal process.
+#'   \item{SILLY_CODE}{Character string, the name of the Silly GCL object.}
+#'   \item{IDs_Removed}{List, a list containing the IDs that were removed from the Silly GCL object. The list is named with the value of 'silly'.}
+#'   \item{is_empty}{Logical, indicating whether the Silly GCL object is now empty after the removal.}
+#'
+#' @details
+#' The 'remove_ids' function removes specific IDs from the .gcl object associated with 'silly'. The function checks if the 'silly' object is empty and raises a stop error if it is empty, as the removal process would not be possible.
+#'
+#' If 'IDs' contains character values, they will be converted to numeric. The function then checks if all the 'IDs' provided are present in the Silly GCL object. If any of the IDs are not found, it raises a stop error.
+#'
+#' After removing the specified IDs from the Silly GCL object, the function gives a message indicating the number of IDs removed and their respective 'silly' object name.
+#'
+#' Additionally, if the Silly GCL object becomes empty after the removal, a warning is issued to notify the user.
+#'
+#' @examples
+#' \dontrun{
+#' password <- "************"
+#' 
+#' create_locuscontrol(markersuite = "Sockeye2011_96SNPs", username = "awbarclay", password = password)
+#' sillyvec <- c("SMCDO03", "SNEVA13")
+#' loki2r(sillyvec = sillyvec, username = "awbarclay", password = password)
+#' remove_ind_miss_loci(sillyvec = sillyvec)
+#' 
+#' remove_ids(silly = "SMCDO03", IDs = 1:10)
+#' }
+#'
+#' @seealso
+#' \code{\link{add_ids}}: Function to add IDs to a Silly GCL object.
+#'
+#' @export
   
+remove_ids <- function(silly, IDs){
   
   if(purrr::is_empty(silly)){
     
