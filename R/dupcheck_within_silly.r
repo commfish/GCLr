4664492ff537669518555cd732890951ee518b28
csjalbert <chase.jalbert@alaska.gov>
2023-08-08 07:00:27 -0800

    added documentation to dupcheck_within_silly; added namespace where missing; fixed formatting/whitespace

--- R/dupcheck_within_silly.r
+++ R/dupcheck_within_silly.r
@@ -1,61 +1,54 @@
-dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quantile = NULL, minnonmissing = 0.6, minproportion = 0.95, ncores = 4, LocusCtl = LocusControl){
-  
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #   This function checks for duplicate individuals within each silly in "sillyvec".
-  #
-  # Inputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #   
-  #   sillyvec - a vector of silly codes without the ".gcl" extention (e.g. sillyvec <- c("KQUART06","KQUART08","KQUART10")). 
-  #
-  #   loci - vector of locus names; if set to NULL all loci in the ".gcl" objects will be used.
-  #
-  #   quantile - this argument along with minproportion are used together to determine the cut-off proportion at which a pair of duplicates 
-  #                                is defined: i.e. proportion = max(quantile(duplication, quantile), minproportion. 
-  #                                Setting "quantile" equal to NULL will skip the calculation of the duplication distribution and will run much faster
-  #
-  #   minnonmissing - the proportion of loci that a pair must share non missing in order to be reported
-  #
-  #   minproportion - the proportion of shared non-missing loci that must be shared between the indivdiuals to be reported as a matching pair, this is passed through to rubias as well 
-  #
-  #   ncores - the number of cores to use in a foreach %dopar% loop. If the number of core exceeds the number on your device, then ncores defaults to detectCores()
-  # 
-  #   LocusCtl - an object created by [GCLr::create_locuscontrol()], (default = LocusControl) 
-  #
-  # Outputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #    When quantile is set to NULL, returns a tibble of duplicate pairs of individuals by silly.
-  #    When quantile is a number, a list containing a tibble of duplicate pairs of individuals by silly and tibble the porportion of duplication for each pair of individuals.
-  #
-  #
-  # Examples~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #
-  # password <- "************"
-  # username <- "******"
-  # create_locuscontrol(markersuite = "Sockeye2011_96SNPs", username = username, password = password)
-  # sillyvec <- c("SMCDO03", "SNEVA13")
-  # loki2r(sillyvec = sillyvec, username = username, password = password)
-  # remove_ind_miss_loci(sillyvec = sillyvec)
-  # pool_collections(c("SMCDO03", "SNEVA13"))
-  # 
-  # dupcheck <- dupcheck_within_silly(sillyvec = "SMCDO03.SNEVA13", loci = LocusControl$locusnames, quantile = 0.99, minproportion = 0.95, ncores = 4)
-  # dupcheckNULLQantile <- dupcheck_within_silly(sillyvec = "SMCDO03.SNEVA13", loci = LocusControl$locusnames, quantile = NULL, minnonmissing = 0.6, minproportion = 0.95, ncores = 4)
-  # 
-  # Note~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #   When quantile is set to NULL this function utilizes rubias::close_matching_samples() to perform the duplicate check and it much faster than when you set a quantile.
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Check for Duplicate Individuals within Each Silly
+#'
+#' This function checks for duplicate individuals within each silly in the provided vector "sillyvec".
+#'
+#' @param sillyvec A vector of silly codes without the ".gcl" extension.
+#' @param loci A vector of locus names. If set to NULL, all loci in the ".gcl" objects will be used.
+#' @param quantile This argument, along with `minproportion`, is used together to determine the cut-off proportion at which a pair of duplicates is defined. Setting "quantile" to NULL will skip the calculation of the duplication distribution and run faster.
+#' @param minnonmissing The proportion of loci that a pair must share non-missing data in order to be reported.
+#' @param minproportion The proportion of shared non-missing loci that must be shared between individuals to be reported as a matching pair. This is passed through to [rubias] as well.
+#' @param ncores The number of cores to use in a [foreach::%dopar%] loop. If the number of cores exceeds the number on your device, then `ncores` defaults to [parallel::detectCores()].
+#' @param LocusCtl An object created by [GCLr::create_locuscontrol]. Default is _LocusControl_.
+#'
+#' @returns When `quantile` is set to NULL, returns a tibble of duplicate pairs of individuals by silly.
+#' When quantile is a number, returns a list containing a tibble of duplicate pairs of individuals by silly
+#' and a tibble with the proportion of duplication for each pair of individuals.
+#'
+#' @examples
+#' \dontrun{
+#' # Set up required objects
+#' password <- "password"
+#' username <- "username"
+#' create_locuscontrol(markersuite = "Sockeye2011_96SNPs", username = username, password = password)
+#' sillyvec <- c("SMCDO03", "SNEVA13")
+#' GCLr::loki2r(sillyvec = sillyvec, username = username, password = password)
+#' GCLr::remove_ind_miss_loci(sillyvec = sillyvec)
+#' GCLr::pool_collections(c("SMCDO03", "SNEVA13"))
+#'
+#' # Check for duplicate individuals
+#' dupcheck <- GCLr::dupcheck_within_silly(sillyvec = sillyvec, loci = LocusControl$locusnames, quantile = 0.99, minproportion = 0.95, ncores = 4)
+#' dupcheckNULLQuantile <- GCLr::dupcheck_within_silly(sillyvec = sillyvec, loci = LocusControl$locusnames, quantile = NULL, minnonmissing = 0.6, minproportion = 0.95, ncores = 4)
+#' }
+#'
+#' @details
+#' When quantile is set to NULL, this function utilizes [rubias::close_matching_samples()] to perform the duplicate check, and it is much faster than when you set a quantile.
+#'
+#' @export
+dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quantile = NULL, minnonmissing = 0.6, minproportion = 0.95, ncores = 4, LocusCtl = LocusControl) {
   
   start.time <- Sys.time() 
   
-  if(!all(loci %in% LocusCtl$locusnames)){
+  if (!all(loci %in% LocusCtl$locusnames)) {
     
     stop(paste0("The following `loci` were not found in `LocusControl`:\n", paste(setdiff(loci, LocusCtl$locusnames), collapse = "\n")))
     
   }
   
-  if(ncores > parallel::detectCores()) {
+  if (ncores > parallel::detectCores()) {
     
     ncores <- parallel::detectCores()  
     
-    }
+  }
   
   nsilly <- length(sillyvec)
   
@@ -65,7 +58,7 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
   
   scores_cols <- sapply(loci, function(locus) {c(locus, paste0(locus, ".1"))}) %>% 
     as.vector()  # This keeps the scores columns in the correct order when there are loci with similar names.
-   
+  
   attr <-
     c(
       "FK_FISH_ID",
@@ -89,7 +82,7 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
       "SillySource"
     )
   
-  sillyvec_new <- silly_n(sillyvec) %>% 
+  sillyvec_new <- GCLr::silly_n(sillyvec) %>% 
     dplyr::filter(n > 1) %>% 
     dplyr::pull(silly)  # Removing single individual collections from sillyvec.
   
@@ -101,7 +94,7 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
     
     maxna <- max(rowSums(is.na(gcl[, scores_cols])))  # What is max number of missing loci?
     
-    if(maxna == 2 * nloci){
+    if (maxna == 2 * nloci) {
       
       stop(paste0("Some of the individuals in sillyvec have no data for all loci. Run remove_ind_miss_loci(sillyvec, loci) prior to checking for duplicate individuals."))
       
@@ -109,11 +102,11 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
     
     # Added this if statement code for haploid markers, rubias::close_matching_samples() was counting them as missing loci because of the NAs in the allele2 column. 
     # This can be removed Eric Anderson fixes the function. 
-    if(any(ploidy == 1)) {
+    if (any(ploidy == 1)) {
       
       haploci <- names(ploidy[ploidy == 1])
       
-      for(locus in haploci){
+      for (locus in haploci) {
         
         gcl[[paste0(locus, ".1")]] <- gcl[[locus]]
         
@@ -130,7 +123,7 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
   `%dopar%` <- foreach::`%dopar%`
   
   # Start if NULL quantile
-  if(is.null(quantile)){
+  if (is.null(quantile)) {
     
     ## Loop through sillys
     cl <- parallel::makePSOCKcluster(ncores)
@@ -172,7 +165,7 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
       new.gcl <- my.gcl[[silly]]
       
       dups <- dupcheck %>% 
-        dplyr::filter(silly==!!silly) %>% 
+        dplyr::filter(silly == !!silly) %>% 
         dplyr::mutate(order = seq(length(silly)), 
                       ID1 = as.numeric(ID1), 
                       ID2 = as.numeric(ID2)
@@ -185,7 +178,7 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
         tidyr::gather(-ID1, key = "Locus", value = "allele") %>% 
         dplyr::group_by(ID1) %>% 
         dplyr::summarize(Missing1 = sum(is.na(allele)), .groups = "drop_last") %>% 
-        dplyr::left_join(dups, by = "ID1")%>% 
+        dplyr::left_join(dups, by = "ID1") %>% 
         dplyr::arrange(order)
       
       ID2 <- new.gcl %>% 
@@ -210,7 +203,7 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
   }  # End if NULL quantile
   
   # Start if quantile 
-  if(!is.null(quantile)){
+  if (!is.null(quantile)) {
     
     cl <- parallel::makePSOCKcluster(ncores)
     
@@ -230,7 +223,7 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
       # Combine allele 1 and 2 into single columns for each locus separated by a period.
       scores0 <- new.gcl[ , c("FK_FISH_ID", scores_cols)] 
       
-      if(n < 2){
+      if (n < 2) {
         
         report <- tibble::tibble(silly = silly, 
                                  ID1 = NA, 
@@ -268,7 +261,7 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
           
         }) %>% purrr::set_names(sapply(1:ncombs, function(comb){
           
-          paste(mycombs[,comb], collapse=".")
+          paste(mycombs[,comb], collapse = ".")
           
         }))
         
@@ -276,7 +269,7 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
         
         dupIND <- duplication >= proportion  # Made this greater than or equal to so it matches the output from rubias::close_matching_samples()
         
-        if(sum(dupIND)){
+        if (sum(dupIND)) {
           
           dups <- data.frame(ID1 = mycombs[1, dupIND], ID2 = mycombs[2, dupIND])
           
@@ -296,7 +289,7 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
             dplyr::select(silly, tidyr::everything())
           
         }
-        if(!sum(dupIND)){
+        if (!sum(dupIND)) {
           
           report <- tibble::tibble(silly = silly, ID1 = NA, ID2 = NA, Missing1 = NA, Missing2 = NA, proportion = NA) 
           
@@ -334,3 +327,5 @@ dupcheck_within_silly <- function(sillyvec, loci = LocusControl$locusnames, quan
   }  # end if quantile
   
 }
+
+  
