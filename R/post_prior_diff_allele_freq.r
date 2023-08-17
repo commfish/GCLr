8dd8fe43bd455f24851900ef7241cfbf3b255c28
awbarclay <andy.barclay@alaska.gov>
2023-08-17 13:55:42 -0800

    Edited for readablity

--- R/post_prior_diff_allele_freq.r
+++ R/post_prior_diff_allele_freq.r
@@ -76,12 +76,12 @@ post_prior_diff_allele_freq <- function(sillyvec, loci, mydir, nchains, mixname)
 
   names(myfiles) <- ChainNames
 
-  myQ <- lapply(myfiles,function(myfile){
+  myQ <- lapply(myfiles, function(myfile){
     
         ans <- t(vapply(myfile, function(strng){
               vec <- strsplit(strng," ")[[1]];
               vec <- vec[vec!=""];
-              vec2 <- rep(NA,max(nalleles)+4);
+              vec2 <- rep(NA, max(nalleles)+4);
               vec2[1:length(vec)] <- vec;
               as.numeric(vec2)
             }, as.numeric(rep(NA, 4+max(nalleles)))));
@@ -108,19 +108,21 @@ post_prior_diff_allele_freq <- function(sillyvec, loci, mydir, nchains, mixname)
 
   names(myQ) <- ChainNames
 
-  myabsdiffsByPopByLocus <- sapply(myQ,function(qq){ 
+  myabsdiffsByPopByLocus <- sapply(myQ, function(qq){ 
     
-    sapply(loci, function(locus){apply(abs(qq[1:C, locus, 1:nalleles[locus]]-rawQ[1:C, locus, 1:nalleles[locus]]), 1, mean) 
+    sapply(loci, function(locus){
+      
+      apply(abs(qq[1:C, locus, 1:nalleles[locus]]-rawQ[1:C, locus, 1:nalleles[locus]]), 1, mean) 
       
       }) 
     
-    },simplify=FALSE)
+    }, simplify = FALSE)
 
   myabsdiffsByPop <- sapply(myabsdiffsByPopByLocus, function(diffs){
     
-    apply(diffs,1,mean)
+    apply(diffs, 1, mean)
     
-    },simplify=FALSE)
+    },simplify = FALSE)
 
   Outliers <- sapply(myabsdiffsByPop, function(popdiffs){
     
