f062ffd2d8557a8cf6ec73bf9cb1206c007479fa
awbarclay <andy.barclay@alaska.gov>
2023-08-17 12:20:32 -0800

    Added an example pairwise Fst matrix to package

---
+++ R/ex_pairwise_fst.R
@@ -0,0 +1,7 @@
+#' Pairwise Fst Matrix
+#' 
+#' This is an example pairwise Fst matrix for creating trees and mds plots
+#' 
+#' @format A named matrix with 14 rows and 14 columns
+#'
+"ex_pairwise_fst"
