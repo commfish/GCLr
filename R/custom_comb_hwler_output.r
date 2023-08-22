f1be20f3d4292975be550929dcccc6c58494c196
awbarclay <andy.barclay@alaska.gov>
2023-08-22 15:22:55 -0800

    custom_comb_hwler_output - added new example and spaces throughout code. Needs to be updated with tidy coding.

--- R/custom_comb_hwler_output.r
+++ R/custom_comb_hwler_output.r
@@ -12,81 +12,95 @@
 #' @param PosteriorOutput A logical value indicating whether to output the posterior values.
 #'
 #' @details 
-#' This function makes all accommodations for the extra baseline group.
+#' This function makes all accommodations for the extra baseline group. 
+#' In addition to summary statistics, this function also produces a pdf file of trace plots for each group and mixture.
 #' 
 #' @return A list of combined HWLER output for each mixture, including summary statistics and posterior output (if requested).
-#'
+#'        
 #' @examples
-#' GCLr::custom_comb_hwler_output()
+#' \dontrun{
+#' setwd("V:/Analysis/2_Central/Sockeye/Cook Inlet/Missing Baseline Analysis") 
+#' 
+#' groupvec <- c(1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 
+#'   3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 5, 4, 4, 5, 5, 
+#'   5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 
+#'   6, 6, 6, 7, 7, 7, 7)
+#' groupnames <- c("A", "B", "C", "D", "E", "F", "G")
+#' 
+#' mixvec <-  c("Western06early", "Western06late")
+#' 
+#' maindir <- "HWLER/output"
+#' 
+#' custom_comb_hwler_output(groupvec = groupvec69, groupnames = groupnames, maindir = maindir, mixvec = mixvec, ext = "BOT", burn = 0.5, alpha = 0.1, PosteriorOutput = TRUE)
+#' }
 #'
 #' @export
-custom_comb_hwler_output=function(groupvec, groupnames, maindir, mixvec, ext="BOT", burn=0.5, alpha=0.1,PosteriorOutput=TRUE){
+custom_comb_hwler_output <- function(groupvec, groupnames, maindir, mixvec, ext = "BOT", burn = 0.5, alpha = 0.1, PosteriorOutput = TRUE){
 
-  G=max(groupvec)
+  G <- max(groupvec)
 
-  groupvec=c(groupvec,G+1)
+  groupvec <- c(groupvec, G+1)
 
-  groupnames=c(groupnames,"GroupX")
+  groupnames <- c(groupnames, "GroupX")
 
-  Results=vector("list",length(mixvec))
-  names(Results)=mixvec
+  Results <- vector("list", length(mixvec))
+  names(Results) <- mixvec
 
-  pdf(file=paste(maindir,"\\TracePlotsHWLER",format(Sys.time(),"%b%d%Y_%H_%M_%S"),".pdf",sep=""),width=11, height=8, family="Helvetica",pointsize=20)
+  pdf(file <- paste(maindir,"\\TracePlotsHWLER", format(Sys.time(), "%b%d%Y_%H_%M_%S"), ".pdf", sep = ""), width = 11, height = 8, family = "Helvetica", pointsize = 20)
 
   for(mix in mixvec){
 
-    HWLERoutput=read.table(paste(maindir,"\\",mix,"Chain1",ext,".",ext,sep=""))[,-1]
+    HWLERoutput <- read.table(paste0(maindir, "/", mix, "/", mix, "Chain1", ext, ".", ext))[,-1]
 
-    nits=nrow(HWLERoutput)
+    nits <- nrow(HWLERoutput)
 
-    Burn=max(1,floor(burn*nits))
+    Burn <- max(1, floor(burn*nits))
 
-    GroupedHWLERoutput=array(NA,c(nits,G+1),dimnames=list(1:nits,groupnames))
+    GroupedHWLERoutput <- array(NA, c(nits, G+1), dimnames = list(1:nits, groupnames))
 
     for(group in groupnames){
 
-      g=match(group,groupnames)
+      g <- match(group, groupnames)
 
-      if(sum(groupvec==g)>1){
+      if(sum(groupvec==g) > 1){
 
-        GroupedHWLERoutput[1:nits,group]=apply(HWLERoutput[1:nits,groupvec==g],1,sum)
+        GroupedHWLERoutput[1:nits,group] <- apply(HWLERoutput[1:nits, groupvec==g], 1, sum)
      
       }      
 
-      if(sum(groupvec==g)==1){
+      if(sum(groupvec==g) == 1){
 
-        GroupedHWLERoutput[1:nits,group]=HWLERoutput[1:nits,groupvec==g]
+        GroupedHWLERoutput[1:nits, group] <- HWLERoutput[1:nits, groupvec==g]
      
       } 
    
-      plot(GroupedHWLERoutput[1:nits,group],type="l",ylim=c(0,1),yaxp=c(0,1,10),col="red",xlab="Iteration",ylab="Proportion",main=paste("Trace of ",group," from ",mix,sep=""))
+      plot(GroupedHWLERoutput[1:nits, group], type = "l", ylim = c(0, 1), yaxp = c(0, 1, 10), col = "red", xlab = "Iteration", ylab = "Proportion", main = paste("Trace of ", group, " from ", mix, sep = ""))
 
-      abline(v=Burn,col="blue")     
+      abline(v = Burn, col = "blue")     
 
     }
 
-    UsedHWLERoutput=GroupedHWLERoutput[(Burn+1):nits,groupnames]
+    UsedHWLERoutput <- GroupedHWLERoutput[(Burn + 1):nits, groupnames]
 
-    Results[[mix]]=vector("list",PosteriorOutput+1)
+    Results[[mix]] <- vector("list", PosteriorOutput + 1)
 
     if(PosteriorOutput){
    
-      names(Results[[mix]])=c("SummaryStats","PosteriorOutput")
+      names(Results[[mix]]) <- c("SummaryStats","PosteriorOutput")
 
-      Results[[mix]][["SummaryStats"]]=data.frame(mean=apply(UsedHWLERoutput,2,mean),sd=apply(UsedHWLERoutput,2,sd),LowCI=apply(UsedHWLERoutput,2,function(clmn){quantile(clmn,alpha/2)}),UpCI=apply(UsedHWLERoutput,2,function(clmn){quantile(clmn,1-alpha/2)})) 
+      Results[[mix]][["SummaryStats"]] <- data.frame(mean = apply(UsedHWLERoutput, 2, mean), sd = apply(UsedHWLERoutput, 2, sd), LowCI = apply(UsedHWLERoutput, 2, function(clmn){quantile(clmn, alpha/2)}), UpCI = apply(UsedHWLERoutput, 2, function(clmn){quantile(clmn, 1-alpha/2)})) 
    
-      Results[[mix]][["PosteriorOutput"]]=UsedHWLERoutput
+      Results[[mix]][["PosteriorOutput"]] <- UsedHWLERoutput
+      
     } 
 
     if(!PosteriorOutput){
    
-      names(Results[[mix]])=c("SummaryStats")
+      names(Results[[mix]]) <- c("SummaryStats")
 
-      Results[[mix]][["SummaryStats"]]=data.frame(mean=apply(UsedHWLERoutput,2,mean),sd=apply(UsedHWLERoutput,2,sd),LowCI=apply(UsedHWLERoutput,2,function(clmn){quantile(clmn,alpha/2)}),UpCI=apply(UsedHWLERoutput,2,function(clmn){quantile(clmn,1-alpha/2)})) 
+      Results[[mix]][["SummaryStats"]] <- data.frame(mean = apply(UsedHWLERoutput, 2, mean), sd = apply(UsedHWLERoutput, 2, sd), LowCI = apply(UsedHWLERoutput, 2, function(clmn){quantile(clmn,alpha/2)}), UpCI = apply(UsedHWLERoutput, 2, function(clmn){quantile(clmn, 1-alpha/2)})) 
    
     }
-
-    
    
   }
 
@@ -95,10 +109,3 @@ custom_comb_hwler_output=function(groupvec, groupnames, maindir, mixvec, ext="BO
   return(Results) 
 
 }
-
-
-
-
-
-
-
