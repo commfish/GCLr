e08cf4817a9e4237b99b0955426a7d3e9d1c74b3
awbarclay <andy.barclay@alaska.gov>
2023-08-22 11:21:00 -0800

    var_comp - updated code to work with new .gcl objects, made tidy, made parallel, output is now a tibble

--- R/var_comp.r
+++ R/var_comp.r
@@ -5,44 +5,60 @@
 #' @param sillyvec A character vector specifying the population silly codes for analysis.
 #' @param loci A character vector of locus names to include in the analysis. Can include haploid markers and/or combined loci.
 #' @param groupvec A numeric vector specifying the reporting group affiliation for each silly code.
-#' @param fstatfile Optional character string specifying the path to an existing FSTAT file (".dat"). If provided, the function verifies compatibility with the input data. Default is NULL.
-#' @param dir A character string specifying the directory where an FSTAT file (".dat") will be created. This argument is used only when `fstatfile` is NULL.
+#' @param fstatfile Optional character string specifying the path to an existing FSTAT file (".dat"). If provided, the function verifies compatibility with the input data. (default = NULL)
 #' @param LocusCtl an object created by [GCLr::create_locuscontrol()], (default = LocusControl)  
 #' 
 #' @details 
-#' The original function is "BackwardEliminationThetaP.GCL" in the temp folder.
+#' Component definitions in the output assume that you are running this function on silly codes that are populations (see Value section). 
+#' This function could also be run on silly codes that are collections, but the definitions in the output would be different.
 #' 
-#' @return A matrix containing the four variance components for each locus:
+#' @return A tibble containing the four variance components for each locus:
 #'   \itemize{
 #'     \item P: For populations (reporting groups in this case).
 #'     \item S: For subpopulations within populations (populations within reporting groups).
 #'     \item I: For individuals within subpopulations (individuals within populations).
 #'     \item G: For alleles within individuals (akin to heterozygosity).
 #'    }
-#'  Component definitions in () assume that you are running this function on silly codes that are populations. This function could also be run on silly codes that are collections, but the definitions in () would be different.
-#' 
+#'    
+##' 
 #' @examples
-#' GCLr::var_comp(sillyvec = Kodiak49Pops, loci = loci90, groupvec = Kodiak49GroupVec9, dir = "V:/WORK/Sockeye/Kodiak/Kodiak Afognak Baseline Look 2014/Kyle/ThetaP")
+#' sillyvec <- GCLr::base2gcl(GCLr::ex_baseline)
+#' 
+#' groupvec <- GCLr::ex_baseline %>% 
+#'   dplyr::group_by(collection) %>% 
+#'   dplyr::summarize(group = unique(repunit)) %>% 
+#'   dplyr::mutate(group = factor(group) %>% as.numeric()) %>% 
+#'   dplyr::pull(group)
+#' 
+#' loci <- GCLr::ex_baseline[,-c(1:5)] %>%
+#'   names() %>%
+#'   gsub(pattern = "*\\.1", x = ., replacement = "") %>%
+#'   unique()
+#' 
+#' GCLr::var_comp(sillyvec = sillyvec, loci = loci, groupvec = groupvec, LocusCtl = GCLr::ex_LocusControl)
 #' 
 #' @references Weir, B.S. 1995. Genetic Data Analysis. Sinauer Associates, Inc. Sunderland, MA. (pg 184)
 #' 
 #' @export
-var_comp <- function(sillyvec, loci, groupvec, fstatfile = NULL, dir, LocusCtl = LocusControl){
+var_comp <- function(sillyvec, loci, groupvec, fstatfile = NULL, LocusCtl = LocusControl, ncores = parallel::detectCores()){
 
   names(sillyvec) <- NULL
   
   nloci <- length(loci)
+  
   nalleles <- LocusCtl$nalleles[loci]
+  
   ploidy <- LocusCtl$ploidy[loci] 
+  
   alleles <- LocusCtl$alleles[loci]
   
   my.gcl <- sapply(sillyvec, function(silly){get(paste(silly, ".gcl", sep = ""), pos = 1)}, simplify = FALSE)
   
-  n <- sapply(sillyvec, function(silly){my.gcl[[silly]]$n})
+  n <- GCLr::silly_n(sillyvec)$n
   
   if(!is.null(fstatfile)) {
     
-    dat0 <- read.fstat.data(fstatfile)
+    dat0 <- hierfstat::read.fstat.data(fstatfile)
     
     if(dim(dat0)[1] != sum(n)) {stop(paste(fstatfile, " does not have the same number of fish as 'sillyvec', verify that the 'fstatfile' corresponds to the fish you want to analyze!"))}
 
@@ -50,77 +66,64 @@ var_comp <- function(sillyvec, loci, groupvec, fstatfile = NULL, dir, LocusCtl =
     
     if(length(unique(dat0$Pop)) != length(groupvec)) {stop("Number of pops in 'fstatfile' is not equal to length(groupvec)!")}
     
+    vars <- names(dat0)
+    
+    dat <- dat0 %>% 
+      tibble::as_tibble() %>% 
+      dplyr::mutate(Region = rep(groupvec, n)) %>% 
+      dplyr::select(Region, dplyr::everything())
+      
   } else {
-    cat("No 'fstatfile' provided, so creating one\n")
+    cat("No 'fstatfile' provided, so creating hierfstat data object\n")
+
+    dat0 <- GCLr::create_hierfstat_data(sillyvec = sillyvec, region = groupvec, pop = seq(length(sillyvec)), loci = loci, ncores = ncores, LocusCtl = LocusCtl) %>% 
+      tibble::as_tibble()
     
-    fstatdir <- paste(dir, "\\", length(groupvec), "pops", nloci, "loci", "_", "fstatfile.dat", sep = "")
-        
-    maxchar <- nchar(nalleles)+1
-    names(maxchar) <- loci
-    
-    nsillys <- length(sillyvec)
-    
-    maxsillychar <- nchar(nsillys)+1
-    
-    scores <- sapply(sillyvec, function(silly){my.gcl[[silly]]$scores[ , loci, ]}, simplify = FALSE)
-    
-    counts <- sapply(sillyvec,function(silly){
-      
-      sapply(1:n[silly],function(i){
-        
-        paste(c(match(silly,sillyvec),sapply(loci,function(locus){
-          
-          ifelse(is.na(scores[[silly]][i, locus, 1]), paste(rep(0, ploidy[locus]*maxchar[locus]), collapse = ""), paste(sapply(1:ploidy[locus], function(allele){
-            
-            paste(c(rep(0, maxchar[locus]-nchar(match(scores[[silly]][i, locus, allele], alleles[[locus]]))), match(scores[[silly]][i, locus, allele], alleles[[locus]])), collapse = "")
-          
-            }),collapse = ""))
-        
-          })), collapse = " ")
-      
-        })
-    
-      }, simplify = FALSE)      
-    
-    fstat <- paste(nsillys, nloci, max(nalleles), max(maxchar), sep = " ")
-    
-    fstat <- rbind(fstat, cbind(loci))
-    
-    fstat <- rbind(fstat, cbind(as.vector(unlist(counts))))
-    
-    write.table(x = fstat, file = fstatdir, row.names = FALSE, col.names = FALSE, quote = FALSE) 
-    
-    dat0 <- read.fstat.data(fstatdir)
+    dat <- dat0 %>% dplyr::select(-spop) %>% 
+      dplyr::rename(Region = region, Pop = pop)
     
   }
-  
-  dat <- data.frame(rep(groupvec, n), dat0)
-  
-  dimnames(dat)[[2]] <- c("Region", "Pop", loci)
   
   G <- max(groupvec)
   
-  VarCompByLocus <- array(0, c(nloci, 4), dimnames = list(loci, c("P", "S", "I", "G")))
-  
   cat("\nCalculating Variance Components for each locus\n", sep = '')
-  if(.Platform$OS.type == "windows") flush.console()
-  pb <- txtProgressBar(min = 0, max = nloci, style = 3)
   
-  for(locus in loci){
-    
-    setTxtProgressBar(pb = pb, value = which(loci == locus))
+  # Multicore loop
+  cl <- parallel::makePSOCKcluster(ncores)
+  
+  doParallel::registerDoParallel(cl, cores = ncores)  
+  
+  `%dopar%` <- foreach::`%dopar%`
+  
+  VarCompByLocus <- foreach::foreach(locus = loci, .packages = c("tidyverse")) %dopar% {
     
     data <- data.frame(dat[ , 1:2], dat[ , locus])
     
-    vc <- varcomp(data, diploid = ploidy[locus]>1)$overall
-    
+    if(ploidy[locus]==2){
+      
+      vc <- c(locus, hierfstat::varcomp(data, diploid = TRUE)$overall) %>% 
+        setNames(c("locus", "P", "S", "I", "G")) %>% 
+        t() %>% 
+        tibble::as_tibble()
+      
+    } else{
+      
+      vc <- c(locus, hierfstat::varcomp(data, diploid = FALSE)$overall) %>% 
+        setNames(c("locus", "P", "S", "I")) %>% 
+        t() %>% 
+        tibble::as_tibble()
+      
+    }
+     
     if(!sum(is.na(vc))){
       
-      VarCompByLocus[locus, 1:length(vc)]=vc
+      vc 
       
     }
     
-  }
+  } %>% dplyr::bind_rows()
+  
+  parallel::stopCluster(cl)
   
   return(VarCompByLocus)
   
