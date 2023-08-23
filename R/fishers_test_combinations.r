07f7f55229f16e636c557f254bb00d084e3bb114
awbarclay <andy.barclay@alaska.gov>
2023-08-23 15:24:05 -0800

    fishers_test_combinations - edited documentation and added spacing to code

--- R/fishers_test_combinations.r
+++ R/fishers_test_combinations.r
@@ -1,6 +1,6 @@
 #' Generate pooling tests
 #' 
-#' This function generates all possible pooling tests for a list of silly codes. The output is plugged into fishers_test(freq = x, loci = x, tests = combos) 
+#' This function generates all possible pooling tests for a list of silly codes. The output is plugged into `GCLr::fishers_test(freq = x, loci = x, tests = combos)` 
 #' 
 #' @param sillys  vector of "silly" elements.
 #' 
@@ -11,16 +11,26 @@
 #' @return a list item containing each of the tests
 #' 
 #' @examples
+#' \dontrun{
 #' silly_elements <- c("silly1", "silly2", "silly3", "silly4", "silly5", "silly6")
 #' min_length <- 2
 #' max_length <- 6
 #' combos <- fisher_combinations(silly_elements, min_length, max_length)
+#' }
+#' 
+#' @seealso [GCLr::fishers_test()]
 #' 
 #' @export
 fisher_combinations <- function(sillys, min_length = 2, max_length) {
+  
   result <- sapply(min_length:max_length, function(n) {
+    
     combs <- t(combn(sillys, n))
+    
     apply(combs, 1, list) %>% unlist(recursive = FALSE)
+    
   })
+  
   unlist(result, recursive = FALSE)
+  
 }
