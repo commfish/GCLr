124ee6826502ea5341b1726ec3df5b651df7b6cf
awbarclay <andy.barclay@alaska.gov>
2023-08-23 14:51:45 -0800

    dupcheck_among_sillys - edited documentation and added missing package names to functions.

--- R/dupcheck_among_sillys.r
+++ R/dupcheck_among_sillys.r
@@ -2,43 +2,37 @@
 #'
 #' This function checks for duplicate individuals between silly code ".gcl" objects. It is mainly used for quality control purposes.
 #'
-#' @param KeySillys A character vector of silly codes without the ".gcl" extension.
+#' @param KeySillys A character vector of silly codes without the ".gcl" extension. These are the sillys with the fish IDs that might be duplicated in `BetweenSillys`
 #' @param KeySillyIDs A named list of character vectors containing FK_FISH_IDs for each silly code in `KeySillys` to check against all silly codes in `BetweenSillys`. If NULL, all FK_FISH_IDs for each silly code in `KeySillys` are checked against all silly codes in `BetweenSillys`.
-#' @param BetweenSillys A character vector of silly codes without the ".gcl" extension.
+#' @param BetweenSillys A character vector of silly codes without the ".gcl" extension. These are the sillys that may contain duplicates. 
 #' @param loci A character vector of locus names. If set to NULL, all loci in the ".gcl" objects will be used.
 #' @param minnonmissing The proportion of loci that a pair must share non-missing genotypes in order to be reported.
 #' @param minproportion The proportion of shared non-missing loci that must be shared between the individuals to be reported as a matching pair.
-#' @param ncores The number of cores to use in a `foreach::%dopar%` loop. If the number of cores exceeds the number on your device, then `ncores` defaults to `parallel::detectCores()`.
-#' @param ncores The number of cores to use in a [foreach::%dopar%] loop. If the number of cores exceeds the number on your device, then `ncores` defaults to [parallel::detectCores()].
+#' @param ncores The number of cores to use in a [foreach::%dopar%] loop. If the number of cores exceeds the number on your device, then `ncores` defaults to `parallel::detectCores()`.
 #' @param plot.results Logical value indicating whether to produce histograms of duplicate rates for each silly code in `KeySillyIDs`.
 #' @param LocusCtl an object created by [GCLr::create_locuscontrol()], (default = LocusControl)  
 #'
 #' @return A tibble that includes the duplicate individuals that exceeded `minproportion` or had the maximum duplicate rate, as well as duplicate individuals that came from the same original collection (i.e., project duplicates). The tibble includes the following variables:
 #'   \itemize{
-#'     \item Keysillyvial: The KeySilly_ID.
-#'     \item Betweensillyvial: The BetweenSilly_ID.
-#'     \item Keymissing: The number of loci without genotypes for each KeySilly_ID.
-#'     \item Betweenmissing: The number of loci without genotypes for each BetweenSilly_ID.
-#'     \item DuplicateRate: The proportion of duplicate genotypes between each KeySilly_ID and BetweenSilly_ID.
+#'     \item \code{Keysillyvial}: The KeySilly_ID.
+#'     \item \code{Betweensillyvial}: The BetweenSilly_ID.
+#'     \item \code{Keymissing}: The number of loci without genotypes for each KeySilly_ID.
+#'     \item \code{Betweenmissing}: The number of loci without genotypes for each BetweenSilly_ID.
+#'     \item \code{DuplicateRate}: The proportion of duplicate genotypes between each KeySilly_ID and BetweenSilly_ID.
 #'   }
 #' The function also prints a histogram of duplicate rates for each silly code in `KeySillyIDs`. Each plot has a vertical red line indicating the `minproportion`, and the highest duplicate rate bar is labeled with the `BetweenSillyIDs` with that duplicate rate. The title of each plot indicates the `KeySillyID` that was checked for duplicates.
-#'
+#' @details
+#' This function is a wrapper for [rubias::close_matching_samples()] and is intended for finding duplicate fish among sillys, mainly for lab quality control analyses to look for extraction/loading errors. To check for duplicates within a silly, use [GCLr::dupcheck_within_silly()]
+#' 
+#' @seealso [GCLr::dupcheck_within_silly()]
+#' 
 #' @examples
-#' password <- "************"
-#' create_locuscontrol(markersuite = "CookInletChinook2013_43SNPs", username = "awbarclay", password = password)
-#' loki2r(sillyvec = c("KKILL05", "KKILL06", "KFUNN05", "KFUNN06"), username = "awbarclay", password = password)
-#' pool_collections(collections = c("KKILL05", "KFUNN05"), IDs = list(KKILL05 = KKILL05.gcl$FK_FISH_ID, KFUNN05 = c(9, 30)), newname = "KKILL05")
-#' pool_collections(collections = c("KFUNN06", "KKILL06"), IDs = list(KFUNN06 = KFUNN06.gcl$FK_FISH_ID, KKILL06 = c(101, 176)), newname = "KFUNN06")
-#' KKILL05.gcl <- KKILL05.gcl %>% mutate(SillySource = case_when(FK_FISH_ID %in% c(69, 70) ~ paste(SILLY_CODE, FK_FISH_ID, sep = "_"), TRUE ~ SillySource))
-#' KFUNN06.gcl <- KFUNN06.gcl %>% mutate(SillySource = case_when(FK_FISH_ID %in% c(184, 185) ~ paste(SILLY_CODE, FK_FISH_ID, sep = "_"), TRUE ~ SillySource))
-#' KKILL06qc.gcl <- KKILL06.gcl %>% mutate(SILLY_CODE = "KKILL06qc") %>% mutate(SillySource = paste(SILLY_CODE, FK_FISH_ID, sep ="_"))
-#' KFUNN05qc.gcl <- KFUNN05.gcl %>% mutate(SILLY_CODE = "KFUNN05qc") %>% mutate(SillySource = paste(SILLY_CODE, FK_FISH_ID, sep ="_"))
-#' KeySillys <- c("KKILL06qc", "KFUNN05qc")
-#' KeySillyIDs <- list(KKILL06qc = c(101, 176), KFUNN05qc = c(9, 30))
-#' BetweenSillys <- c("KKILL05", "KKILL06", "KFUNN05", "KFUNN06")
-#' loci <- LocusControl$locusnames
-#' results <- GCLr::dupcheck_among_sillys(KeySillys = c("KKILL06qc", "KFUNN05qc"), KeySillyIDs = list(KKILL06qc = c(101, 176), KFUNN05qc = c(9, 30)), BetweenSillys = c("KKILL05", "KKILL06", "KFUNN05", "KFUNN06"), loci = LocusControl$locusnames, minnonmissing = 0.6, minproportion = 0.9, ncores = 4)
-#'
+#' \dontrun{
+#'   
+#'   GCLr::dupcheck_among_sillys(KeySillys = c("KKILL06qc", "KFUNN05qc"), KeySillyIDs = list(KKILL06qc = c(101, 176), KFUNN05qc = c(9, 30)), BetweenSillys = c("KKILL05", "KKILL06", "KFUNN05", "KFUNN06"), loci = LocusControl$locusnames, minnonmissing = 0.6, minproportion = 0.9, ncores = 20) 
+#'   
+#' }
+#'                                                                                                                                                                                                                                                                                                           
 #' @export
 dupcheck_among_sillys <- function(KeySillys, KeySillyIDs = NULL, BetweenSillys, loci, minnonmissing = 0.6, minproportion = 0.9, ncores = 4, plot.results = TRUE, LocusCtl = LocusControl){
 
@@ -50,10 +44,9 @@ dupcheck_among_sillys <- function(KeySillys, KeySillyIDs = NULL, BetweenSillys, 
   
   if(sum(KeySillys %in% BetweenSillys) > 1){
     
-    stop("One or more of the KeySillys are also included in BetweenSillys. This is not the intended use of this function. Use dupcheck_within_silly() to find duplicates within a silly.")
+    stop("One or more of the KeySillys are also included in BetweenSillys. This is not the intended use of this function. Use GCLr::dupcheck_within_silly() to find duplicates within a silly.")
     
     }
-    
   
   if(ncores > parallel::detectCores()) {
     
@@ -63,7 +56,7 @@ dupcheck_among_sillys <- function(KeySillys, KeySillyIDs = NULL, BetweenSillys, 
   
   ploidy <- LocusCtl$ploidy[loci]
   
-  scores_cols <- sapply(loci, function(locus) {c(locus, paste0(locus, ".1"))}) %>% 
+  scores_cols <- sapply(loci, function(locus){c(locus, paste0(locus, ".1"))}) %>% 
     as.vector()  # This keeps the scores columns in the correct order when there are loci with similar names.
   
   # my.key
@@ -72,7 +65,7 @@ dupcheck_among_sillys <- function(KeySillys, KeySillyIDs = NULL, BetweenSillys, 
     my.key <- lapply(KeySillys, function(silly){
       
       get(paste0(silly, ".gcl")) %>% 
-        filter(FK_FISH_ID %in% KeySillyIDs[[silly]])
+        dplyr::filter(FK_FISH_ID %in% KeySillyIDs[[silly]])
       
     }) %>% bind_rows()
       
@@ -91,7 +84,7 @@ dupcheck_among_sillys <- function(KeySillys, KeySillyIDs = NULL, BetweenSillys, 
     
     get(paste0(silly, ".gcl"))
     
-  }) %>% set_names(BetweenSillys)
+  }) %>% purrr::set_names(BetweenSillys)
   
   # Added this if statement code for haploid markers, rubias::close_matching_samples() was counting them as missing loci because of the NAs in the allele2 column. 
   # This can be removed Eric Anderson fixes the function. 
@@ -162,14 +155,14 @@ dupcheck_among_sillys <- function(KeySillys, KeySillyIDs = NULL, BetweenSillys, 
         dplyr::filter(DuplicateRate == max(DuplicateRate))
       
       plot <- dc %>% 
-        ggplot2::ggplot(aes(x = DuplicateRate, fill = Betweensilly)) + # I added "fill = BetweenSilly" so the histogram will show which between sillys make up the distribution - this may be removed if others don't like it.
+        ggplot2::ggplot(ggplot2::aes(x = DuplicateRate, fill = Betweensilly)) + # I added "fill = BetweenSilly" so the histogram will show which between sillys make up the distribution - this may be removed if others don't like it.
         ggplot2::geom_histogram(bins = 100) +
         ggplot2::geom_vline(xintercept = minproportion, color = "red", size = 1.25)+
         ggplot2::xlab("Duplicate Rate")+
         ggplot2::ylab("Frequency") +
         ggplot2::xlim(0, 1.02) +
         ggplot2::ggtitle(label = paste0("KeySillyID: ", key))+
-        ggplot2::geom_text(aes(x = max_dup$DuplicateRate %>% unique, y = length(max_dup$DuplicateRate)), label = paste0(max_dup$Betweensillyvial, collapse = "_"), angle = 90, hjust = -.05)
+        ggplot2::geom_text(ggplot2::aes(x = max_dup$DuplicateRate %>% unique, y = length(max_dup$DuplicateRate)), label = paste0(max_dup$Betweensillyvial, collapse = "_"), angle = 90, hjust = -.05)
       
       suppressWarnings(print(plot))
       
@@ -194,7 +187,7 @@ dupcheck_among_sillys <- function(KeySillys, KeySillyIDs = NULL, BetweenSillys, 
   
   Keymissing <- my.key %>% 
     dplyr::select(Keysillyvial = SillySource, all_of(scores1)) %>% 
-    dplyr::mutate(across(all_of(scores1), is.na)) %>% 
+    dplyr::mutate(dplyr::across(all_of(scores1), is.na)) %>% 
     dplyr::group_by(Keysillyvial) %>% 
     dplyr::mutate(Keymissing = sum(!!!syms(scores1))) %>% 
     dplyr::select(Keysillyvial, Keymissing)
@@ -202,8 +195,8 @@ dupcheck_among_sillys <- function(KeySillys, KeySillyIDs = NULL, BetweenSillys, 
   Betweenmissing <- my.between %>% 
     dplyr::bind_rows() %>% 
     dplyr::filter(SillySource %in% dupcheck0$Betweensillyvial) %>% 
-    dplyr::select(Betweensillyvial = SillySource, all_of(scores1)) %>% 
-    dplyr::mutate(across(all_of(scores1), is.na)) %>%  
+    dplyr::select(Betweensillyvial = SillySource, tidyselect::all_of(scores1)) %>% 
+    dplyr::mutate(dplyr::across(all_of(scores1), is.na)) %>%  
     dplyr::group_by(Betweensillyvial) %>% 
     dplyr::mutate(Betweenmissing = sum(!!!syms(scores1))) %>% 
     dplyr::select(Betweensillyvial, Betweenmissing)
