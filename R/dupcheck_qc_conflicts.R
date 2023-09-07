78fc75a0ea73248b61cea0a87a6bb0a52b7c0253
awbarclay <andy.barclay@alaska.gov>
2023-09-07 14:17:22 -0800

    dupcheck_qc_conflicts - fixed issue in documentation

--- R/dupcheck_qc_conflicts.R
+++ R/dupcheck_qc_conflicts.R
@@ -5,12 +5,12 @@
 #'  
 #'  @param conflicts a tibble containing 3 variables:
 #'  \iemize{
-#'    \item \code(SillySource) original silly code and fish ID for each individual with conflicts
-#'    \item \code(n) the number of conflicts among loci
-#'    \item \code(p) the proportion of conflicts among loci
+#'    \item \code{SillySource} original silly code and fish ID for each individual with conflicts
+#'    \item \code{n} the number of conflicts among loci
+#'    \item \code{p} the proportion of conflicts among loci
 #'  }
 #'  
-#'  @param conflict_rate the conflict rate for determining which samples to investigate. This is assinged at the beginging of the QC script.
+#'  @param conflict_rate the conflict rate for determining which samples to investigate. This is assigned at the beginning of the QC script.
 #'  @param project_sillys a character vector of silly codes included in the project
 #'  @param LocusCtl an object created by [GCLr::create_locuscontrol()], (default = `LocusControl`)
 #'  @param ncores the number of cores to run in parallel (default = [parallel::detectCores()])
