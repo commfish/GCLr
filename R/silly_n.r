33f88cfb772695d8348b8d54b0909c6eb6f1940a
awbarclay <andy.barclay@alaska.gov>
2023-09-18 10:19:48 -0800

    silly_n - added reproducible example

--- R/silly_n.r
+++ R/silly_n.r
@@ -7,18 +7,16 @@
 #' @returns A two-column tibble containing the silly codes (silly) and their corresponding sample sizes (n).
 #'
 #' @examples
-#' \dontrun{
-#' # Load the necessary data
-#' load("V:/Analysis/2_Central/Chinook/Cook Inlet/2019/2019_UCI_Chinook_baseline_hap_data/2019_UCI_Chinook_baseline_hap_data.RData")
-#'
-#' # Get sample sizes for specific silly codes
-#' silly_n(sillyvec = sillyvec157)
-#' }
-#'
-#'
+#'  
+#' sillyvec <- GCLr::base2gcl(GCLr::ex_baseline)
+#' 
+#' GCLr::silly_n(sillyvec = sillyvec)
+#' 
 #' @export
 silly_n <- function(sillyvec) {
+  
   lapply(sillyvec, function(silly){
+    
     my.gcl <- get(paste0(silly, ".gcl"))
     
     if (!tibble::is_tibble(my.gcl)) {
@@ -28,6 +26,7 @@ silly_n <- function(sillyvec) {
     }
     
     tibble::tibble(silly = silly, n = dim(my.gcl)[[1]])
-  }) %>% 
-    dplyr::bind_rows()
+    
+  }) %>% dplyr::bind_rows()
+  
 }
