26eeb05ea1458cadf22882d2e181a643095d4c93
awbarclay <andy.barclay@alaska.gov>
2023-09-18 13:27:26 -0800

    save_nexus_color_tree - added reproducible example and minor edits

--- R/save_nexus_color_tree.r
+++ R/save_nexus_color_tree.r
@@ -1,17 +1,35 @@
-#' Save Nexus file that includes a phylogenetic tree. 
+#' Save Nexus File that includes a Phylogenetic Tree. 
 #'
 #' This function is a wrapper for [ape::write.nexus()] and creates a Nexus format tree file including tip labels and branch colors, which can be opened and modified in FigTree. FigTree can be downloaded from GitHub: [FigTree Releases](https://github.com/rambaut/figtree/releases)
 #'
-#' @param tree a phylogenetic tree object (class = "phylo") produced by the \pkg{ape} package
+#' @param tree a phylogenetic tree object (`class = "phylo"`) produced by the \pkg{ape} package
 #' @param file the file path with file name including .nex extension
 #' @param groupvec a numeric vector indicating the group affiliation of each tip label
-#' @param group.col a character vector of R [colors()] with a length of \code{max(groupvec)} to color the branches
+#' @param group.col a character vector of R [grDevices::colors()] with a length of `max(groupvec)` to color the branches
 #' @param tip.labs A character vector with the same length as groupvec to label the tips of the tree. tip.labs cannot have spaces to work in FigTree. If tip.labs contains spaces, this function will replace them with an underscore.
 #' @param sep Delimiter used to replace spaces in tip.labs. If spaces are left in tip.labs (called TAXALABLES in the NEXUS file) when FigTree opens the file, an error message will pop up saying the number of taxa is greater than ntaxa.
 #'
 #' @examples
-#' load("V:/Analysis/2_Central/Chinook/Susitna River/Susitna_Chinook_baseline_2020/Susitna_Chinook_baseline_2020.Rdata")
-#' save_nexus_color_tree(tree = FstTree, file = "Susitna_Chinook_tree.nex", groupvec = groupvec, group.col = grcol, tip.labs = Final_Pops$location)
+#' 
+#' groupvec <- GCLr::ex_baseline %>%
+#'   dplyr::group_by(collection) %>%
+#'   dplyr::filter(dplyr::row_number()==1) %>%
+#'   dplyr::pull(repunit) %>%
+#'   factor() %>%
+#'   as.numeric()
+#' 
+#' sillyvec <- GCLr::base2gcl(ex_baseline)
+#' 
+#' loci <- GCLr::ex_baseline[,-c(1:5)] %>%
+#'   names() %>%
+#'   gsub(pattern = "*\\.1", x = ., replacement = "") %>%
+#'   unique()
+#' 
+#' pwfst <- GCLr::pairwise_fst(sillyvec = sillyvec, loci = loci, inputfile = system.file("genepop", "ex_genepop.txt", package = "GCLr"), ncores = parallel::detectCores())
+#' 
+#' FstTree <- ape::nj(pwfst)
+#' 
+#' GCLr::save_nexus_color_tree(tree = FstTree, file = path.expand("~/example_tree.nex"), groupvec = groupvec, group.col = c("green", "red", "blue"), tip.labs = paste0("pop", 1:14))
 #' 
 #' @export
 save_nexus_color_tree <- function(tree, file, groupvec, group.col, tip.labs, sep = "_"){
@@ -40,8 +58,6 @@ save_nexus_color_tree <- function(tree, file, groupvec, group.col, tip.labs, sep
 
   ape::write.nexus(phy = tree, file = file)
 
-  
-    
   if(is.numeric(group.col)){
   
     group.col <- colors()[group.col]
