cac2baf34ec6b267023edf3d6c9de61d5d413af8
awbarclay <andy.barclay@alaska.gov>
2023-09-18 14:04:57 -0800

    save_sillys - added reproducible example and some minor edits

--- R/save_sillys.r
+++ R/save_sillys.r
@@ -1,17 +1,26 @@
 #' Save sillys as R objects
 #'
-#' This function saves sillys as R objects with `dput`, it is a wrapper for `dput`.
+#' This function saves sillys as R objects with [base::dput()] or [base::saveRDS()].
 #'
 #' @param sillyvec character vector of sillys you wish to save
 #' @param path character vector of where you want to save sillys
 #' @param rds logical; if set to TRUE the sillys will be saved in rds format,
 #'        default is FALSE for backwards compatibility.
 #'
-#' @return All sillyvec are saved as "sillyvec.txt" or "sillyvec.rds", the .gcl is removed
+#' @return All sillyvec are saved as "sillyvec.txt" or "sillyvec.rds", the .gcl is removed.
 #'
+#' @seealso [GCLr::save_objects()]
+#' @seealso [base::dput()]
+#' @seealso [base::saveRDS()]
+#' 
 #' @examples
-#' iris <- iris  # bringing into pos = 1
-#' save_sillys(sillyvec = "SGILL15D15", path = "Raw genotypes")
+#' sillyvec <- GCLr::base2gcl(GCLr::ex_baseline)
+#' 
+#' dir.create(path.expand("~/Raw genotypes"))
+#' 
+#' save_sillys(sillyvec = sillyvec, path = path.expand("~/Raw genotypes"), rds = FALSE)
+#' 
+#' save_sillys(sillyvec = sillyvec, path = path.expand("~/Raw genotypes"), rds = TRUE)
 #'
 #' @export
 save_sillys <- function(sillyvec, path, rds = FALSE) {
@@ -19,18 +28,20 @@ save_sillys <- function(sillyvec, path, rds = FALSE) {
   sillyvec.gcl <- paste0(sillyvec, ".gcl")
   
   if (!all(sillyvec.gcl %in% ls(, pos = 1))) {
-    stop(paste0(
-      "These sillys:\n",
-      paste(setdiff(sillyvec.gcl, ls(, pos = 1)), collapse = "\n"),
-      "\nare not in your workspace, hoser!!!"
-    ))
+    
+    stop(paste0("These sillys:\n", paste(setdiff(sillyvec.gcl, ls(, pos = 1)), collapse = "\n"), "\nare not in your workspace, hoser!!!"))
+    
   }
   
   empty <- sapply(sillyvec, function(silly) {
     if (rds == FALSE) {
+      
       dput(get(paste0(silly, ".gcl")), paste0(path, "/", silly, ".txt"))
+      
     } else {
+      
       saveRDS(get(paste0(silly, ".gcl")), paste0(path, "/", silly, ".rds"))
+      
     }
     
   })
