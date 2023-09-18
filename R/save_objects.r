f1cc36781565060083a1a594155c259bdda9b031
awbarclay <andy.barclay@alaska.gov>
2023-09-18 13:49:07 -0800

    save_objects - added new reproducible example and minor edits to documentation and code.

--- R/save_objects.r
+++ R/save_objects.r
@@ -1,21 +1,29 @@
-#' Save R objects to disk
+#' Save R Objects to Disk
 #'
-#' This function saves R objects with either `dput` or `saveRDS`.
+#' This function saves R objects with either [base::dput()] or [base::saveRDS()].
 #'
-#' `dput` saves object code to a text file.
-#' `saveRDS` saves objects in rds format, which is a compressed format - saves and reads back into R much faster than a text file.
+#' [base::dput()] saves object code to a text file.
+#' [base::saveRDS()] saves objects in rds format, which is a compressed format - saves and reads back into R much faster than a text file.
 #'
 #' @param objects character vector of objects you wish to save
 #' @param path character vector of where you want to save objects
-#' @param rds logical; if set to TRUE the objects will be saved in rds format,
-#'        default is FALSE for backwards compatibility.
+#' @param rds logical; if set to `TRUE` the objects will be saved in rds format,
+#'        default is `FALSE` for backwards compatibility.
 #' 
-#' @return All objects are saved as "objects.txt" or "objects.rds"
+#' @return All objects are saved as "object.txt" or "object.rds"
+#' 
+#' @seealso [base::dput()]
+#' @seealso [base::saveRDS()]
 #'
 #' @examples
-#' iris <- iris  # bringing into pos = 1
-#' save_objects(objects = "iris", path = "Objects", rds = TRUE)
-#'
+#' sillyvec <- GCLr::base2gcl(GCLr::ex_baseline)
+#' 
+#' dir.create(path.expand("~/objects"))
+#' 
+#' save_objects(objects = paste0(sillyvec, ".gcl"), path = path.expand("~/objects"), rds = TRUE)
+#' 
+#' save_objects(objects = paste0(sillyvec, ".gcl"), path = path.expand("~/objects"), rds = FALSE)
+#' 
 #' @export
 save_objects <- function(objects, path, rds = FALSE) {
   
@@ -29,14 +37,23 @@ save_objects <- function(objects, path, rds = FALSE) {
   }
   
   empty <- sapply(objects, function(obj) {
+    
     x <- get(obj)
+    
     if (!is.null(attr(x = x, which = "problems"))) {
+      
       attr(x = x, which = "problems") <- NULL
+      
     }  # resolves issue with readr::read_csv not playing nicely with dget if attr-problems exist
-    if (rds == FALSE) {
+    
+    if (rds == FALSE) {.
+      
       dput(x, paste0(path, "/", obj, ".txt"))
+      
     } else {
+      
       saveRDS(x, paste0(path, "/", obj, ".rds"))
+      
     }
     
   })
