38f945209cdb7d71510200d5a9c78026af5fcbb8
awbarclay <andy.barclay@alaska.gov>
2024-02-23 14:50:46 -0900

    IA_thresholds - updated for consistency with other functions and added a rate argument

--- R/IA_thresholds.R
+++ R/IA_thresholds.R
@@ -1,15 +1,17 @@
 #' Get assignment threshold by reporting group
 #' 
-#' This function takes the unmodified output from `rubias::self_assign()`, 
-#' calculates true positive rate and precision, and returns a tibble of minimum
+#' This function takes the unmodified output from [rubias::self_assign], 
+#' calculates recall (aka true positive rate) and precision, and returns a tibble of minimum
 #' and maximum assignment thresholds for filtering top individual assignment results 
 #' in order to meet the desired true positive rate and precision.
 #' 
-#' @param input the unmodified output object from `rubias::self_assign`
+#' @param sa_input the unmodified output object from  [rubias::self_assign]
 #' 
-#' @param tpr_cutoff the true positive rate cutoff; default = 0.80
+#' @param rates optional; the unmodified output object from [GCLr::loo_roc_rate_calc] created using the supplied `sa_input`. If left `NULL`, the rates will be calculated from `sa_input`. Supplying a rates object will make this function run a lot faster.
+#'
+#' @param min_rec the recall cutoff; default = 0.80
 #' 
-#' @param pre_cutoff the precision cutoff; default = 0.95
+#' @param min_pre the precision cutoff; default = 0.95
 #' 
 #' @param min_thres minimum assignment threshold to include in the output; default = 0.50
 #' 
@@ -20,34 +22,40 @@
 #' The assignments for each sampled are filtered for the reporting group with the highest assignment (top assignment). An assignment threshold 
 #' is then used to filter out samples with lower top assignments to reduce the number of incorrect assignments. This function takes the leave-one-out 
 #' results from `rubias::self_assign()`and summarizes the number of true positives (TP), false negatives (FN), false positives (FP) at different assignment thresholds,
-#' and calculates the true positive rate (TPR) and Precision at each threshold and returns a tibble of minimum and maximum assignment thresholds for
-#' each reporting group where `TPR >= tpr_cutoff`, `Precision >= pre_cutoff`, and the `assignment threshold >= min_thres`.
-#' 
-#' \itemize{ 
-#'          \item \eqn{TPR = TP / (TP + FN)}
+#' and calculates the recall and precision at each threshold and returns a tibble of minimum and maximum assignment thresholds for
+#' each reporting group where `Recall >= min_rec`, `Precision >= min_pre`, and the `assignment threshold >= min_thres`. 
+#' TPR and precision are are calculated from the loo results with [GCLr::loo_roc_rate_calc] and defined as follows:\itemize{ 
+#'          \item \eqn{Recall = TP / (TP + FN)} Recall is also know at the true positive rate.
 #'          \item \eqn{Precision = TP / (TP + FP)}
+#'          \item {Where, *TP* = number of true positive top assignments, 
+#'          *FN* = number of false negative top assignments, 
+#'          *FP* = number of false positive top assignments from the leave-one-out analysis}
 #'          }
 #' 
-#' @returns a tibble with the re
+#' @returns a tibble with the minimum and maximum threshold for each repunit (aka reporting group)
 #' 
 #' @examples
-#' input <- rubias::self_assign(reference = GCLr::ex_baseline, gen_start_col = 5)
+#' sa_input <- rubias::self_assign(reference = GCLr::ex_baseline, gen_start_col = 5)
 #' 
-#' assignment_thresholds(input = input, tpr = 0.80, pre = 0.95, min_thres = 0.50, thres_levels = seq(0.01, 0.99, by = 0.01))
+#' assignment_thresholds(input = input, rates = NULL, min_rec = 0.80, min_pre = 0.95, min_thres = 0.50, thres_levels = seq(0.01, 0.99, by = 0.01))
 #' 
 #' @export
-IA_thresholds <- function(input, tpr_cutoff = 0.80, pre_cutoff = 0.95, min_thres = 0.50, thres_levels = seq(0.01, 0.99, by = 0.01)){
+IA_thresholds <- function(sa_input, rates = NULL, min_rec = 0.80, min_pre = 0.95, min_thres = 0.50, thres_levels = seq(0.01, 0.99, by = 0.01)){
   
-  group_names <- input$repunit %>% 
+  group_names <- sa_input$repunit %>% 
     unique()
   
-  rates <- GCLr::loo_roc_rate_calc(data = input, thres_levels = seq(0.01, 0.99, by = 0.01), group_names = group_names, ncores = parallel::detectCores())
+  if(is.null(rates)){
+    
+    rates <- GCLr::loo_rate_calc(data = sa_input, thres_levels = seq(0.01, 0.99, by = 0.01), group_names = group_names, ncores = parallel::detectCores())
+    
+  }
     
   output <- rates %>% 
-      dplyr::filter(level >= min_thres, tpr >= tpr_cutoff, pre >= pre_cutoff) %>% 
+      dplyr::filter(level >= min_thres, tpr >= min_rec, pre >= min_pre) %>% 
       dplyr::group_by(repunit) %>% 
       dplyr::summarize(min_threshold = min(level), max_threshold = max(level))
   
   return(output)
 
-  }
+}
