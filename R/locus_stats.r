91c36bfbdf9ce8c125531eb2e50d5dbf49616399
awbarclay <andy.barclay@alaska.gov>
2025-06-09 12:19:23 -0800

    locus_stats - added option to supply a hierfstat data object to make function run faster. Also added observed gene diversity and allelic richness to the output.

--- R/locus_stats.r
+++ R/locus_stats.r
@@ -2,7 +2,7 @@
 #' 
 #' @description
 #' This function calculates basic locus statistics (Ho, Fis, Fst) for each locus across collections (silly code).
-#'
+#' 
 #' @param sillyvec A character vector of silly codes without the ".gcl" extension.
 #' @param loci A character vector of locus names.
 #' @param ncores A numeric value for the number of cores to use in a \pkg{foreach} `%dopar%` loop (default = 4). 
@@ -13,12 +13,17 @@
 #'     \itemize{
 #'       \item \code{locus}: locus name
 #'       \item \code{Ho}: observed heterozygosity calculated by [hierfstat::basic.stats()]
+#'       \item \code{Hs}: observed gene diversity calculated by [hierfstat::basic.stats()]
+#'       \item \code{Ar}: allelic richness calculated by [hierfstat::allelic.richness()]
 #'       \item \code{Fis}: Fis calculated by [hierfstat::varcomp()]
 #'       \item \code{Fst}: Fst calculated by [hierfstat::varcomp()]
 #'       }       
 #'       
 #' @details
-#' This function uses [hierfstat::hierfstat()] to calculate locus statistics for Ho, Fis, and Fst.
+#' This function uses [hierfstat::hierfstat()] to calculate locus statistics for Ho, Hs, Ar, Fis, and Fst. If a hierfstat data object is supplied (recommended), sillyvec and loci objects
+#' are not needed, and the function runs a lot faster.  If a hierfstat data object is not supplied (i.e., data = NULL) and sillyvec and loci objects are supplied, the
+#' function will create a temporary hierfstat data object to calculate the locus statistics. The hierfstat data object produced by this function is not assigned to your workspace, so it's best 
+#' to create a data object using [GCLr::create_hierfstat_data()] if you want to use the object for something else.
 #' 
 #' @seealso 
 #' [hierfstat::hierfstat()]
@@ -29,12 +34,18 @@
 #' @examples
 #'   
 #' sillyvec <- GCLr::base2gcl(GCLr::ex_baseline)
-#'   
-#' GCLr::locus_stats(sillyvec = sillyvec, loci = GCLr::ex_LocusControl$locusnames[-c(10, 12, 13, 32, 33)], ncores = parallel::detectCores(), LocusCtl = GCLr::ex_LocusControl )
+#' 
+#' loci <- ((get(paste0(sillyvec[1], ".gcl")) %>% names())[-c(1:19)])[seq(1, 185, by = 2)]
+#' 
+#' dat <- GCLr::create_hierfstat_data(sillyvec, region = NULL, pop = seq_along(sillyvec), loci = loci, ncores = parallel::detectCores(), LocusCtl = GCLr::ex_LocusControl)
+#' 
+#' GCLr::locus_stats(data = dat, sillyvec = NULL, loci = NULL, ncores = parallel::detectCores(), LocusCtl = GCLr::ex_LocusControl)#Supplying data object
+#'
+#' GCLr::locus_stats(data = NULL, sillyvec = sillyvec, loci = loci, ncores = parallel::detectCores(), LocusCtl = GCLr::ex_LocusControl)#Supplying sillyvec and loci
 #' 
 #' @export
-locus_stats <- function(sillyvec, loci, ncores = 4, LocusCtl = LocusControl) {
-    
+locus_stats <- function(data = NULL, sillyvec = NULL, loci = NULL, ncores = parallel::detectCores(), LocusCtl = LocusControl) {
+  
   if(ncores > parallel::detectCores()) {
     
     stop("'ncores' is greater than the number of cores available on machine\nUse 'detectCores()' to determine the number of cores on your machine")
@@ -45,8 +56,22 @@ locus_stats <- function(sillyvec, loci, ncores = 4, LocusCtl = LocusControl) {
   
   ploidy <- LocusCtl$ploidy[loci]
   
-  dat <- GCLr::create_hierfstat_data(sillyvec = sillyvec, region = NULL, pop = seq_along(sillyvec), loci = loci, ncores = ncores, LocusCtl = LocusCtl)
-
+  if(!is.null(data)){
+    
+    dat <- data
+    
+    loci <- names(data)[-c(1:2)]
+    
+  } else(
+    
+    if(is.null(sillyvec)|is.null(loci)){
+      
+      stop("sillyvec and loci must be supplied when data = NULL.")
+      
+      }else( dat <- GCLr::create_hierfstat_data(sillyvec = sillyvec, region = NULL, pop = seq_along(sillyvec), loci = loci, ncores = ncores, LocusCtl = LocusCtl))
+    
+    )
+ 
   cl <- parallel::makePSOCKcluster(ncores)
   
   doParallel::registerDoParallel(cl, cores = ncores) #Start cluster  
@@ -96,16 +121,34 @@ locus_stats <- function(sillyvec, loci, ncores = 4, LocusCtl = LocusControl) {
   
   MyTable <- dplyr::bind_rows(MyTable0, Overall) 
   
-  #Heterozygosities
+  #Observed Heterozygosity
   Hovec <- apply(hierfstat::basic.stats(dat[, c("pop", loci[ploidy==2])])$Ho, 1, mean, na.rm = TRUE)
   
   Ho <- tibble::tibble(locus = c(loci[ploidy==2], "Overall"), Ho = c(Hovec, mean(Hovec, na.rm = TRUE)))
   
-  #Join varcomp summary with Ho  
+  #Observed gene diversity
+  Hsvec <- apply(hierfstat::basic.stats(dat[, c("pop", loci[ploidy==2])])$Hs, 1, mean, na.rm = TRUE)
+  
+  Hs <- tibble::tibble(locus = c(loci[ploidy==2], "Overall"), Hs = c(Hsvec, mean(Hsvec, na.rm = TRUE)))
+  
+  #Allelic richness
+  Ar <- lapply(loci, function(locus){
+    
+    my.dat <- dat %>% dplyr::select(pop, spop, all_of(locus))
+    
+    x <- apply((hierfstat::allelic.richness(my.dat, diploid = ploidy[locus]==2)$Ar)[locus,], 1, mean)
+    
+    tibble::tibble(locus = locus, Ar = x)
+    
+  }) %>% dplyr::bind_rows()
+  
+  #Join varcomp summary with Ho, Hs, and Ar
   output <- MyTable %>% 
     dplyr::left_join(Ho, by = "locus") %>% 
+    dplyr::left_join(Hs, by = "locus") %>% 
+    dplyr::left_join(Ar, by = "locus") %>% 
     dplyr::mutate(Fis = I, Fst = P) %>% 
-    dplyr::select(locus, Ho, Fis, Fst)
+    dplyr::select(locus, Ho, Hs, Ar, Fis, Fst)
   
   print(Sys.time() - start.time)
 
