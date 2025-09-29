43261c8311153c05dce684a67e21c8e940a15a9a
awbarclay <andy.barclay@alaska.gov>
2025-09-29 12:13:52 -0800

    locus_stats - updated to account for data files that contain only 1 population and population that have fixed loci

--- R/locus_stats.r
+++ R/locus_stats.r
@@ -80,67 +80,76 @@ locus_stats <- function(data = NULL, sillyvec = NULL, loci = NULL, ncores = para
  
   ploidy <- LocusCtl$ploidy[loci]
   
-  ##### Weir and Cockerham (1984) Fst and Fis. Note: this has to be done for each locus separately to extract the variance components needed for calculating overall FST
-  cl <- parallel::makePSOCKcluster(ncores)
+  # Check the number of pops in the data. If only 1 pop, 
+  npops <- dat$pop %>% 
+    unique() %>% 
+    length()
   
-  doParallel::registerDoParallel(cl, cores = ncores) #Start cluster  
-  
-  `%dopar%` <- foreach::`%dopar%`
-  
-  WC_per.loc <- foreach::foreach(locus = loci, .packages = c("hierfstat", "magrittr", "tibble", "dplyr")) %dopar% {
-    
-    p <- ploidy[locus]==2
-    
-    # Check if the locus is fixed. Fixed loci will cause the wc function to bomb because there is zero variation. 
-    nalleles <- dat[ ,locus] %>% 
-      unique() %>% 
-      na.omit() %>% 
-      as.vector() %>% 
-      length()
-    
-    # If a locus is fixed, make Fst, Fis, and variance components hard zero 
-    if(nalleles == 1){
-      
+  # If npops is greater than 1 calculate Fis and Fst using hierftat::wc, else set Fst = NA and use hierfstat::basic.stats for FIS.
 
-      tibble::tibble(locus = locus, Fst = 0, Fis = 0, a = 0, b = 0, c = 0) %>% 
-        dplyr::mutate(total_var = sum(a, b, c, na.rm = TRUE)) 
-      
-    }else{
-      
-      loc.wc <- hierfstat::wc(dat[, c("pop", locus)], diploid = p)
-      
-      var_comps <- loc.wc$sigma %>% 
-        tibble::as_tibble() %>% 
-        dplyr::filter(loc == 1)
-      
-      a_comp <- ifelse(p == TRUE, var_comps$siga[1], sum(var_comps$siga))# Among populations variance
-      
-      b_comp <- ifelse(p == TRUE, var_comps$sigb[1], sum(var_comps$sigb))# Among individuals within populations variance
-      
-      c_comp <- ifelse(p == TRUE, var_comps$sigw[1], 0) # Within-individual variance; Haploid loci don't have this component
-      
-      tibble::tibble(locus = locus, Fst = loc.wc$FST, Fis = loc.wc$FIS, a = a_comp, b = b_comp, c = c_comp) %>% 
-        dplyr::mutate(total_var = sum(a, b, c, na.rm = TRUE)) 
+  if(npops > 1){ ############################### More than 1 pop #####################################################
     
-    }
+    ##### Weir and Cockerham (1984) Fst and Fis. Note: this has to be done for each locus separately to extract the variance components needed for calculating overall FST
+    cl <- parallel::makePSOCKcluster(ncores)
     
-  } %>% dplyr::bind_rows()
-  
-  parallel::stopCluster(cl)# Stop cluster
-  
-  #Calculate overall WC Fst and Fis
-  hap.loci <- loci[ploidy == 1] #FIS cannot be calculated for haploid loci, so those will be excluded from the output for that calculation
-  
-  Fis_overall <- WC_per.loc %>% 
-    dplyr::filter(!locus %in% hap.loci) %>% 
-    dplyr::summarise(Fis = sum(b)/(sum(b)+sum(c)))
+    doParallel::registerDoParallel(cl, cores = ncores) #Start cluster  
     
-  WC_overall <- WC_per.loc %>% 
-    dplyr::summarise(locus = "Overall", Fst = sum(a)/sum(total_var)) %>% 
-    dplyr::mutate(Fis = Fis_overall$Fis)
-  
-  WC <- dplyr::bind_rows(WC_per.loc %>% dplyr::select(locus, Fis, Fst), WC_overall)
-  
+    `%dopar%` <- foreach::`%dopar%`
+    
+    WC_per.loc <- foreach::foreach(locus = loci, .packages = c("hierfstat", "magrittr", "tibble", "dplyr")) %dopar% {
+      
+      p <- ploidy[locus]==2
+      
+      # Check if the locus is fixed. Fixed loci will cause the wc function to bomb because there is zero variation. 
+      nalleles <- dat[ ,locus] %>% 
+        unique() %>% 
+        na.omit() %>% 
+        as.vector() %>% 
+        length()
+      
+      
+      # If a locus is fixed, make Fst, Fis, and variance components hard zero 
+      if(nalleles == 1){
+        
+        tibble::tibble(locus = locus, Fst = 0, Fis = 0, a = 0, b = 0, c = 0) %>% 
+          dplyr::mutate(total_var = sum(a, b, c, na.rm = TRUE)) 
+        
+      }else{
+        
+        loc.wc <- hierfstat::wc(ndat = dat[, c("pop", locus)], diploid = p)
+        
+        var_comps <- loc.wc$sigma %>% 
+          tibble::as_tibble() %>% 
+          dplyr::filter(loc == 1)
+        
+        a_comp <- ifelse(p == TRUE, var_comps$siga[1], sum(var_comps$siga))# Among populations variance
+        
+        b_comp <- ifelse(p == TRUE, var_comps$sigb[1], sum(var_comps$sigb))# Among individuals within populations variance
+        
+        c_comp <- ifelse(p == TRUE, var_comps$sigw[1], 0) # Within-individual variance; Haploid loci don't have this component
+        
+        tibble::tibble(locus = locus, Fst = loc.wc$FST, Fis = loc.wc$FIS, a = a_comp, b = b_comp, c = c_comp) %>% 
+          dplyr::mutate(total_var = sum(a, b, c, na.rm = TRUE)) 
+        
+      }
+      
+    } %>% dplyr::bind_rows()
+    
+    parallel::stopCluster(cl)# Stop cluster
+    
+    #Calculate overall WC Fst and Fis
+    hap.loci <- loci[ploidy == 1] #FIS cannot be calculated for haploid loci, so those will be excluded from the output for that calculation
+    
+    Fis_overall <- WC_per.loc %>% 
+      dplyr::filter(!locus %in% hap.loci) %>% 
+      dplyr::summarise(Fis = sum(b)/(sum(b)+sum(c)))
+    
+    WC_overall <- WC_per.loc %>% 
+      dplyr::summarise(locus = "Overall", Fst = sum(a)/sum(total_var)) %>% 
+      dplyr::mutate(Fis = Fis_overall$Fis)
+    
+    WC <- dplyr::bind_rows(WC_per.loc %>% dplyr::select(locus, Fis, Fst), WC_overall)
+    
   ##### Observed Heterozygosity and Expected Heterozygosity (aka Gene Diversity)
   if(any(ploidy==2)){ # Diploid
     
@@ -205,11 +214,85 @@ locus_stats <- function(data = NULL, sillyvec = NULL, loci = NULL, ncores = para
   
   Ar <- tibble::tibble(locus = c(loci, "Overall"), Ar = c(Arvec, mean(Arvec, na.rm = TRUE)))
   
-  ##### Join varcomp summary with Ho, Hs, and Ar
+  ##### Join WC, H, and Ar
   output <- WC %>% 
     dplyr::left_join(H, by = "locus") %>% 
     dplyr::left_join(Ar, by = "locus") %>% 
     dplyr::select(locus, Ho, Hs, Ar, Fis, Fst)
+  
+  }else{ ############################### Only 1 pop #####################################################
+    
+    ##### Observed Heterozygosity and Expected Heterozygosity (aka Gene Diversity)
+    if(any(ploidy==2)){ # Diploid
+      
+      dip.loci <- loci[ploidy == 2]
+      
+      BS_dip <- hierfstat::basic.stats(dat[, c("pop", dip.loci)], diploid = TRUE)$perloc %>% 
+        tibble::as_tibble(rownames = "locus") %>% 
+        dplyr::select(locus, Ho, Hs, Fis, Fst)
+      
+    }else{BS_dip <- NULL}
+    
+    if(any(ploidy==1)){ # Haploid
+      
+      hap.loci <- loci[ploidy == 1]
+      
+      BS_hap <- hierfstat::basic.stats(dat[, c("pop", hap.loci)], diploid = FALSE)$perloc %>% 
+        tibble::as_tibble(rownames = "locus") %>% 
+        dplyr::select(locus, Ho, Hs, Fis, Fst)
+      
+      if(length(hap.loci)== 1){ # The structure of basic.stats output is different for a single haploid locus, so have treat those differently.
+        
+        BS_hap <- BS_hap[1,] %>% 
+          dplyr::mutate(locus = hap.loci)
+        
+      }
+      
+    }else{BS_hap <- NULL}
+    
+    BS0 <- dplyr::bind_rows(BS_dip, BS_hap) %>% 
+      dplyr::mutate(Fst = NA)
+    
+    BS_overall <- BS0 %>% 
+      dplyr::summarize(locus = "Overall", Fst = NA, Fis = mean(Fis, na.rm = TRUE), Ho = mean(Ho, na.rm = TRUE),  Hs = mean(Hs, na.rm = TRUE))
+    
+    BS <- dplyr::bind_rows(BS0, BS_overall)
+    
+    ##### Allelic richness
+    if(any(ploidy==2)){ # Diploid
+      
+      Arvec_dip <- hierfstat::allelic.richness(dat[, c("pop", loci[ploidy == 2])], diploid = TRUE)$Ar %>% 
+        rowMeans( na.rm = TRUE)
+      
+    }else{Arvec_dip <- NULL}
+    
+    if(any(ploidy==1)){ # Haploid
+      
+      hap.loci <- loci[ploidy == 1]
+      
+      Arvec_hap <- hierfstat::allelic.richness(dat[, c("pop", hap.loci)], diploid = FALSE)$Ar %>% 
+        rowMeans(na.rm = TRUE)
+      
+      # When there is a single haploid locus, hierfstat::allelic.richness inserts a dummy.loc as a placeholder and also replaces any dashes in the locus name with periods.
+      # This if statement drops the dummy.loc and sets the correct locus name.
+      if(length(hap.loci)==1){ 
+        
+        Arvec_hap <- purrr::set_names(Arvec_hap[1], hap.loci) 
+        
+      }
+      
+    }else{Arvec_hap <- NULL}
+    
+    Arvec <- c(Arvec_dip, Arvec_hap)[loci]
+    
+    Ar <- tibble::tibble(locus = c(loci, "Overall"), Ar = c(Arvec, mean(Arvec, na.rm = TRUE)))
+    
+    ##### Join BS and Ar
+    output <- BS %>% 
+      dplyr::left_join(Ar, by = "locus") %>% 
+      dplyr::select(locus, Ho, Hs, Ar, Fis, Fst)
+    
+  }
   
   print(Sys.time() - start.time)
 
